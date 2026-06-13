237d4585806221565ace0b74b6daae63f92a18a4
kapibara001 <trostikshop@gmail.com>
2026-06-13 17:42:37 +0500

    13 june update

--- fortests.py
+++ fortests.py
@@ -1,3 +1,61 @@
+def binary_search(arr, t):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        middle = (left + right) // 2
+        if arr[middle] == t:
+            return middle
+        else:
+            if t > arr[middle]:
+                left = middle
+            if t < arr[middle]:
+                right = middle
+    return -1
+print(f"Бинарный поиск 6 в [1, 2, 3, 4, 5, 6, 7]: {binary_search([1, 2, 3, 4, 5, 6, 7], 6)}")
+
+
+phi = (1+5**0.5)/2
+def golden_search(arr, t, L=0, R=None):
+    if R is None:
+        R = len(arr) -1
+
+    CR = L + int((R-L)/phi)
+    CL = R - int((R-L)/phi)
+
+    if t == arr[CL]:
+        return CL
+    if t == arr[CR]:
+        return CR
+
+    if t > arr[CL]:
+        return golden_search(arr, t, CL+1, R)
+    elif t < arr[CR]:
+        return golden_search(arr, t, L, CR-1)
+    else:
+        return golden_search(arr, t, CL+1, CR-1)
+print(f"Поиск золотым сечением target=3, arr=[1, 2, 3, 4, 5]: {golden_search([1, 2, 3, 4, 5], 3)}")
+
+
+def checking_bracket(s):
+    bracket = {
+        '}': '{',
+        ')': '(',
+        ']': '[',
+    }
+    openings = set(bracket.values())
+    stack = []
+
+    for i in s:
+        if i in openings:
+            stack.append(i)
+        else:
+            if stack.pop() != bracket[i]:
+                return False
+
+    return not stack
+print(f"Проверка строки '[][)', '[][]': {checking_bracket('[][)')}, {checking_bracket('[][]')}")
+
+
 def hanoi(a, b, c, n):
     if n == 1:
         print(a, '->', c)
@@ -5,6 +63,8 @@ def hanoi(a, b, c, n):
         hanoi(a, c, b, n-1)
         print(a, '->', c)
         hanoi(b, a, c, n-1)
+print("Ханойские башни:")
+hanoi("A", "B", "C", 3)
 
 
 def polish_math(s):
@@ -29,6 +89,7 @@ def polish_math(s):
 
     if len(stack) == 1:
         return stack[0]
+print(f"Польская инверсная запись: {polish_math('1 2 + 4 /')}")
 
 
 def hyperWithoutFor(a, b, n):
@@ -38,6 +99,7 @@ def hyperWithoutFor(a, b, n):
         return a
 
     return hyperWithoutFor(a, hyperWithoutFor(a, b-1, n), n-1)
+print(f"Гипероператор без цикла (рекурсия): {hyperWithoutFor(2, 3, 3)}")
 
 
 def hyperWithFor(a, b, n):
@@ -48,6 +110,7 @@ def hyperWithFor(a, b, n):
         for _ in range(b-1):
             s = hyperWithFor(a, s, n-1)
         return s
+print(f"Гипероператор с циклом и рекурсией: {hyperWithFor(2, 3, 3)}")
 
 
 def words(s, r):
@@ -57,11 +120,4 @@ def words(s, r):
         for i in s:
             for j in words(s, r-1):
                 yield i+j
-
-
-print("Ханойские башни:")
-hanoi("A", "B", "C", 3)
-print(f"Польская инверсная запись: {polish_math('1 2 + 4 /')}")
-print(f"Гипероператор без цикла (рекурсия): {hyperWithoutFor(2, 3, 3)}")
-print(f"Гипероператор с циклом и рекурсией: {hyperWithFor(2, 3, 3)}")
 print(f"Слова произвольной длины: {list(words("ABC", 2))}")
